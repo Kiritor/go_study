315d5f708a2746363ad04d6c5c78925a0f63a143
LCore <kiritor@hotmail.com>
2015-04-01 13:33:19 +0800

    玩家实体对象

--- cgss/src/cg/player.go
+++ cgss/src/cg/player.go
@@ -9,15 +9,15 @@ import (
 */
 type Player struct {
     Name string "name"
-	Level int "level"
-	Exp int "exp"
+    Level int "level"
+    Exp int "exp"
 	Room int "room"
-	mq chan *Message //等待收取的信息
+    mq chan *Message // 等待收取的消息
 }
 
 func NewPlayer() *Player {
 	m := make(chan *Message,1024)
-	player :=&Player{"",0,0,m}
+	player :=&Player{"",0,0,0,m}
 
 	go func(p *Player) {
 	   for {
