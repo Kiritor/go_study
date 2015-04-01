ea0d2150f416bcd306de17e3ed796b796138c0aa
LCore <kiritor@hotmail.com>
2015-04-01 13:32:43 +0800

    修改进程间通信框架client

--- cgss/src/ipc/client.go
+++ cgss/src/ipc/client.go
@@ -16,17 +16,20 @@ func NewIpcClient(server *IpcServer) *IpcClient {
 
 func (client *IpcClient)Call(method,params string) (resp *Response,err error) {
 	req := &Request{method,params}
+
 	var b []byte
 	    b,err = json.Marshal(req)
 	if err != nil {
 		return
 	}
 	client.conn <-string(b)
-	fmt.Println(b)
+	fmt.Println(string(b))
 	str :=<-client.conn //等待返回值
 	var resp1 Response
 	err = json.Unmarshal([]byte(str),&resp1)
+
 	resp = &resp1
+
 	return
 }
 
